8db306995f9c4b7a5f8818635122a5e5db7c550d
袁嘉宝 <331954685@qq.com>
2026-08-20 13:32:41 +0800

    ...

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+let a = document.getElement('a')
+let b = document.getElement('b')
 function a() {
-    alert(<center><div>请点击要访问的文件</div><button><a href="/ai/1game.html">1game</a></button><br><button><a href="/ai/2game.html">2game</a></button></center>)
+    a.style.display = 'block'
 }
 function b() {
-    alert(<center><div>请点击要访问的文件</div><button><a href="/My/01.html">01</a></button></center>)
+    b.style.display = 'block'
 }
