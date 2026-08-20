be097a1f7401a69c4fec71fafaefa17d8fe56900
袁嘉宝 <331954685@qq.com>
2026-08-20 13:03:17 +0800

    ......

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
-let input = document.getElementById('input');
-let i = 0;
-function ok() {
-    while (i = 1) {
-        switch (input.value) {
-            case 'My/01.html':
-                window.open('My/01.html')
-                i++
-                break;
-            case 'ai/1game.html':
-                window.open('ai/1game.html')
-                i++
-                break;
-            case 'ai/2game.html':
-                window.open('ai/2game.html')
-                i++
-                break;
-            default:
-                alert('请输入有效的路径!');
-                break;
-        }
-    }
+let div = document.getElementById(div)
+let diva = document.getElementById(diva)
+function 1() {
+    alert(div)
+}
+function 2() {
+    alert(diva)
 }
