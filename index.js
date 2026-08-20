b0246c38e093a7c5024286c49daba200b1c61ac5
袁嘉宝 <331954685@qq.com>
2026-08-20 13:15:50 +0800

    ..

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-let div = document.getElementById(div)
-let diva = document.getElementById(diva)
 function a() {
-    alert(div.innerHTML)
+    alert('<center><div>请点击要访问的文件</div><button><a href="/ai/1game.html">1game</a></button><br><button><a href="/ai/2game.html">2game</a></button></center>')
 }
 function b() {
-    alert(diva.innerHTML)
+    alert('<center><div>请点击要访问的文件</div><button><a href="/My/01.html">01</a></button></center>')
 }
