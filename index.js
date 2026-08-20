ac36aeb7082fd52c1b8ae5657b05b09312e48981
袁嘉宝 <331954685@qq.com>
2026-08-20 13:17:58 +0800

    ..

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function a() {
-    alert('<center><div>请点击要访问的文件</div><button><a href="/ai/1game.html">1game</a></button><br><button><a href="/ai/2game.html">2game</a></button></center>')
+    alert(<center><div>请点击要访问的文件</div><button><a href="/ai/1game.html">1game</a></button><br><button><a href="/ai/2game.html">2game</a></button></center>)
 }
 function b() {
-    alert('<center><div>请点击要访问的文件</div><button><a href="/My/01.html">01</a></button></center>')
+    alert(<center><div>请点击要访问的文件</div><button><a href="/My/01.html">01</a></button></center>)
 }
