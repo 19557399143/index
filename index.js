62a8083a46cf571d2a9237742ad29b11eae548cb
袁嘉宝 <331954685@qq.com>
2026-08-20 13:11:25 +0800

    ...

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let div = document.getElementById(div)
 let diva = document.getElementById(diva)
 function a() {
-    alert(div)
+    alert(div.innerHTML)
 }
 function b() {
-    alert(diva)
+    alert(diva.innerHTML)
 }
