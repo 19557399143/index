087ec2c283230d5e18a61bef4e24d631e8d996d7
袁嘉宝 <331954685@qq.com>
2026-09-12 10:11:59 +0800

    .

---
+++ My/03/login/js.js
@@ -0,0 +1,36 @@
+let thref = window.location.href
+switch (thref) {
+    case '19557399143.github.io/index/My/03/login/':
+        log()
+        break;
+
+    case '19557399143.github.io/index/My/03/login/in/':
+        in()
+        break;
+    case '19557399143.github.io/index/My/03/hello/'
+        he()
+        break;
+}
+const users = {}
+function log() {
+    const in1 = document.getElementById('in1')
+    const in2 = document.getElementById('in2')
+    
+    function ok() {
+        const int = `users.${in1.value}`
+        if(int&&in2.value === `${int}.mim`){
+            const oen = window.open(`19557399143.github.io/index/My/03/hello/?${in1.value}`)
+        }
+    }
+    function cliit(){
+        const open = window.open('19557399143.github.io/index/My/03/login/in/')
+    }
+}
+function in() {
+    
+}
+function he() {
+    const h = document.getElementById('h')
+    h.value = window.location.search
+    const open = window.open('19557399143.github.io/index/My/03/hello/')
+}
